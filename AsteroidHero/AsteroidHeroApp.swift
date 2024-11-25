986f3f10a2b8d78cd4461dc960f371bb7738c74d
Marcel Jäger <marcel.jaeger.a@outlook.com>
2024-11-25 15:43:23 +0100

    Initial Commit

---
+++ AsteroidHero/AsteroidHeroApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsteroidHeroApp.swift
+//  AsteroidHero
+//
+//  Created by Marcel Jäger on 25.11.24.
+//
+
+import SwiftUI
+
+@main
+struct AsteroidHeroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AsteroidHero/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AsteroidHero
+//
+//  Created by Marcel Jäger on 25.11.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
