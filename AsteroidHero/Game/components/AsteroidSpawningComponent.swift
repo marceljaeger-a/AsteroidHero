4ce2b020169310ac63cd08081eb69cf5d939797d
Marcel Jäger <marcel.jaeger.a@outlook.com>
2024-11-27 18:25:18 +0100

    Add min distance between Asteorides

--- AsteroidHero/Game/components/AsteroidSpawningComponent.swift
+++ AsteroidHero/Game/components/AsteroidSpawningComponent.swift
@@ -11,9 +11,14 @@ import SpriteKit
 
 class AsteroidSpawningComponent: GKComponent {
     let scene: GameScene
+    
+    //Spawning Parameter
     var spawingRate: TimeInterval = 5
     var spawningCount: Int = 1
     var asteoridSpeed: CGFloat = 20
+    var asteoridMinDistance: Int = 20
+    var asteoridSpawningPadding: Int = 10
+    
     private var secondsBuffer: TimeInterval = 0
     
     init(scene: GameScene) {
@@ -30,23 +35,37 @@ class AsteroidSpawningComponent: GKComponent {
     
         if secondsBuffer > spawingRate {
             
-            //Spawn
-            for i in 0..<spawningCount {
-                spawnAsteroid()
-            }
-            
+            spawnAsteroid()
             secondsBuffer = 0
+            
         }
     }
     
     func spawnAsteroid() {
         let y = scene.frame.maxY + 10
-        let xRange = Int(scene.frame.minX)+10...Int(scene.frame.maxX)-10
-        
-        let x = CGFloat(xRange.randomElement() ?? xRange.lowerBound)
-        let startPoint = CGPoint(x: x, y: y)
-        let movementVector = CGVector(dx: 0, dy: -1 * asteoridSpeed)
-        
-        _ = AsteoridEntity(position: startPoint, movementVector: movementVector, healthPoints: 1, attackDamage: 1, scene: scene)
+        let xRange = (Int(scene.frame.minX.rounded(.up)) + asteoridSpawningPadding)...(Int(scene.frame.maxX.rounded(.down)) - asteoridSpawningPadding)
+        var xSet = Set(xRange)
+   
+        for _ in 0..<spawningCount {
+            
+            if xSet.isEmpty {
+                break
+            }
+            
+            if let xRandom = xSet.randomElement() {
+                
+                let x = CGFloat(xRandom)
+                
+                let xRemovingRange = (xRandom - asteoridMinDistance)...(xRandom + asteoridMinDistance)
+                xSet.subtract(xRemovingRange)
+                
+                let startPoint = CGPoint(x: x, y: y)
+                let movementVector = CGVector(dx: 0, dy: -1 * asteoridSpeed)
+                
+                _ = AsteoridEntity(position: startPoint, movementVector: movementVector, healthPoints: 1, attackDamage: 1, scene: scene)
+                
+            }
+            
+        }
     }
 }
